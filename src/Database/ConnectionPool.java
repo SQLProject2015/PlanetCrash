4693ee0d5f8c17611f18c9e24e6e0929bab41014
yuvalb <drakemare@gmail.com>
2015-05-24 15:23:13 +0300

    Connection pooling prototype

--- src/Database/ConnectionPool.java
+++ src/Database/ConnectionPool.java
@@ -2,6 +2,7 @@ package Database;
 
 import java.sql.*;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /*
@@ -13,31 +14,33 @@ public class ConnectionPool {
 	private static final String USER = "DbMysql14"; //TODO: change this
 	private static final String PASS = "DbMysql14"; //TODO: change this
 
-	private static final int POOLMAX = 5;
-	private static final int POOLMIN = 3;
-	
-	private AtomicInteger count;
-	
+	private AtomicInteger POOLMAX = new AtomicInteger(5);
+	private AtomicInteger POOLMIN = new AtomicInteger(3);
+
+	private AtomicInteger count; //how many connections have been spawned overall
+
 	private Stack<Connection> cStack;
 	
+	private AtomicBoolean destroyed = new AtomicBoolean(false); //was this pool assigned destruction?
+
 	public ConnectionPool() {
 		this.cStack = new Stack<Connection>();
 		this.count.set(0);
-		
+
 		//populate stack
-		while(cStack.size()<POOLMIN)
+		while(cStack.size()<POOLMIN.get())
 			try {
 				cStack.push(createConnection());
 			} catch (DatabaseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		
+
 	}
-	
-	private Connection createConnection() throws DatabaseException {
+
+	private synchronized Connection createConnection() throws DatabaseException {
 		Connection newConn;
-		
+
 		// loading the driver
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -53,16 +56,64 @@ public class ConnectionPool {
 		} catch (SQLException e) {
 			throw new DatabaseException("Unable to connect - " + e.getMessage());
 		}
-		System.out.println("Connected!");
+
+		if(newConn!=null) {
+			System.out.println("Connected!");
+			count.incrementAndGet();
+		}
 		return newConn;
 	}
-	
-	public synchronized Connection getConnection() {
-		return null;
+
+	public synchronized Connection getConnection() throws DatabaseException {
+		if(destroyed.get()) //if pool was destroyed, no new connections allowed!
+			return null;
+		if(cStack.size()==0 && count.get()<POOLMAX.get()) { //stack is empty but we can still spawn more
+			cStack.push(createConnection());
+			return cStack.pop();
+		} else if (cStack.size()>0) { //we have ready connectons
+			return cStack.pop();
+		} else { //stack is empty and all pools full
+			return null; //TODO: should we wait for a new one instead?
+		}
 	}
-	
+
 	public synchronized void disposeConnection(Connection connection) {
-		//TODO: return connection to pool
+		if(count.get()<POOLMAX.get())
+			cStack.push(connection);
+		else
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	/*
+	 * Destroys all current connections and makes sure all returned ones are destroyed
+	 */
+	public synchronized void destroyPool() {
+		if(destroyed.get()) //was already issued destruction
+			return;
+		destroyed.set(true);
+		POOLMAX.set(0);
+		POOLMIN.set(0);
+
+		while(cStack.size()>0) //empty stack
+			try {
+				destroyConnection(cStack.pop());
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+
+	private synchronized void destroyConnection(Connection connection) throws SQLException {
+		connection.close();
+		count.decrementAndGet();
 	}
 	
+	public synchronized boolean isDestroyed() {
+		return destroyed.get();
+	}
 }
--- src/Database/DatabaseHandler.java
+++ src/Database/DatabaseHandler.java
@@ -4,51 +4,59 @@ import java.sql.*;
 
 public class DatabaseHandler {
 
+	ConnectionPool cPool;
 	Connection conn;
 
 	private static final String CONNPATH = "jdbc:mysql://host_addr:port/schema_name"; //"jdbc:mysql://localhost:3306/dbexample"; //TODO: change this
 	private static final String USER = ""; //TODO: change this
 	private static final String PASS = ""; //TODO: change this
 
-	public DatabaseHandler() {
+	public DatabaseHandler(ConnectionPool connPool) {
+		this.cPool=connPool;
 		
 		//Try and open connection
 		try{
-			this.conn=openConnection();
+			//while connection pool is active and we can't get a connection, continue trying
+			while((!cPool.isDestroyed())&&(this.conn=cPool.getConnection())==null) 
+				Thread.sleep(1500);
 		} catch (DatabaseException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
 	}
 
-	/*
-	 * Opens a new connection to the database
-	 */
-	private Connection openConnection() throws DatabaseException {
-		
-		Connection newConn;
-		
-		// loading the driver
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			throw new DatabaseException("Unable to load the MySQL JDBC driver..\n"+e.getMessage());
-		}
-		System.out.println("Driver loaded successfully");
-
-		// creating the connection
-		System.out.print("Trying to connect... ");
-		try {
-			newConn = DriverManager.getConnection(CONNPATH,USER,PASS);
-		} catch (SQLException e) {
-			throw new DatabaseException("Unable to connect - " + e.getMessage());
-		}
-		System.out.println("Connected!");
-		return newConn;
-	}
+//	/*
+//	 * Opens a new connection to the database
+//	 */
+//	private Connection openConnection() throws DatabaseException {
+//		
+//		Connection newConn;
+//		
+//		// loading the driver
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//		} catch (ClassNotFoundException e) {
+//			throw new DatabaseException("Unable to load the MySQL JDBC driver..\n"+e.getMessage());
+//		}
+//		System.out.println("Driver loaded successfully");
+//
+//		// creating the connection
+//		System.out.print("Trying to connect... ");
+//		try {
+//			newConn = DriverManager.getConnection(CONNPATH,USER,PASS);
+//		} catch (SQLException e) {
+//			throw new DatabaseException("Unable to connect - " + e.getMessage());
+//		}
+//		System.out.println("Connected!");
+//		return newConn;
+//	}
 
 	
 	public void close() throws SQLException {
-		this.conn.close();
+		this.cPool.disposeConnection(conn);
 	}
 }
