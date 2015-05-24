9ccdaec292d47f5963d91470813c9510fc54f10d
yuvalb <drakemare@gmail.com>
2015-05-24 13:39:27 +0300

    Parser+Lexer+jdbc jar

---
+++ src/Database/Yago/AbstractYagoParser.java
@@ -0,0 +1,29 @@
+package Database.Yago;
+
+import java.io.FileNotFoundException;
+
+public abstract class AbstractYagoParser {
+	
+	String filepath; //path of the yago TSV file
+
+	/*
+	 * Gets a TSV filepath and parses all its rows to populate the DB
+	 */
+	public AbstractYagoParser(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	public void populate() throws FileNotFoundException {
+		YagoLexer lexer = new YagoLexer(filepath);
+		
+		while(lexer.hasNext())
+			parse(lexer.next());
+		
+	}
+	
+	/*
+	 * This function gets a yago entry and uses it to populate the database
+	 */
+	abstract public void parse(YagoEntry toParse);
+
+}
