08427f6fdc16673dcb65cc9b0d2cdfe0410fc34d
Tomer <tomehtomeh@gmail.com>
2015-05-24 15:55:06 +0300

    countries parser

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.FileNotFoundException;
+
+import Database.Yago.parse_countries;
+
 
 
 public class Main {
-
+	public static void main(String args[]) throws FileNotFoundException{
+		parse_countries c = new parse_countries("D:\\db project\\yago3_tsv\\yagoTransitiveType.tsv");
+		c.populate();
+	}
+	
 }
