565ad7d8bf1ecf9640de626c6b8ef6e6e28f3d3a
dor shalom <dshalom@gmail.com>
2015-06-13 18:39:00 +0300

    manual updater is now working fully

--- src/Database/Updates/ManualUpdater.java
+++ src/Database/Updates/ManualUpdater.java
@@ -51,9 +51,6 @@ public class ManualUpdater {
 	}
 	
 	public static boolean update_country(String name, String continent, String currency, String language, String capital, int population_size, DatabaseHandler dbh) throws SQLException, NotFoundException{
-		// get capital id from DB
-		int idCaptitalCity = getIdFromDB("City", "idCity", capital, dbh);
-
 		// get continent id from DB
 		int idContinent = getIdFromDB("Continent", "idContinent", continent, dbh);
 		
@@ -62,14 +59,58 @@ public class ManualUpdater {
 		
 		// get language id from DB
 		int idLanguage = getIdFromDB("Language", "idLanguage", language, dbh);
-
-		dbh.singleInsert(String.format("%s.Country",conf.get_db_name()), new String[]{"`Name`","`idContinent`","`idCurrency`","`idLanguage`","`idCapital`","`PopulationSize`","`isManual`"},
-				new Object[]{name, idContinent, idCurrency, idLanguage,idCaptitalCity, population_size,1});
+		
+		// get capital id from DB
+		int idCaptitalCity = getIdFromDB("City", "idCity", capital, dbh);
+		
+		//get country id from DB
+		int idCountry = getIdFromDB("Country", "idCountry", name, dbh);
+		
+		String query = String.format("UPDATE %s.Country SET idContinent=%d,idCurrency=%d,"
+									+ "idLanguage=%d, idCapital=%d, PopulationSize=%d, isManual=1 WHERE idCountry=%d",
+									conf.get_db_name(),idContinent,idCurrency,idLanguage,idCaptitalCity,population_size,idCountry);
+		dbh.executeUpdate(query);
 		
 		return true;
 	}
 	
-	
+	/** 
+	 * returns an Object[] array with details in the following order: continent, currency, language, capitalCity, populationSize
+	 * @throws SQLException 
+	 * @throws NotFoundException 
+	 */
+	public static Object[] get_country_details(String countryName, DatabaseHandler dbh) throws SQLException, NotFoundException{
+		//get details from the db (by id's)
+		
+		String query = String.format("SELECT * FROM %s.Country WHERE Name=\"%s\";", conf.get_db_name(),countryName);
+		ResultSet rs = dbh.executeQuery(query);
+		int idContinent=0, idLanguage=0, idCurrency=0 , idCapital=0, populationSize=0;
+		if (rs.first()) {
+			idContinent = rs.getInt(3);
+			idCurrency = rs.getInt(4);
+			idLanguage = rs.getInt(5);
+			idCapital = rs.getInt(6);
+			populationSize = rs.getInt(7);
+		}
+		else{
+			throw new NotFoundException(String.format("Country %s not found in DB", countryName));
+		}
+		
+		// get continent name from DB
+		String continent = getNameFromDB("Continent", "Name", idContinent, dbh);
+		
+		// get currency name from DB
+		String currency = getNameFromDB("Currency", "Name", idCurrency, dbh);
+		
+		// get language name from DB
+		String language = getNameFromDB("Language", "Name", idLanguage, dbh);
+		
+		// get capital name from DB
+		String capitalCity = getNameFromDB("City", "Name", idCapital, dbh);
+		
+		return new Object[]{continent, currency, language, capitalCity, populationSize};
+	}
+
 	public static int getIdFromDB(String tableName, String column, String valueToSearch, DatabaseHandler dbh) throws NotFoundException, SQLException{
 		ResultSet rs;
 		int retId = 0;
@@ -82,5 +123,21 @@ public class ManualUpdater {
 		return retId;
 		
 	}
-	
+	private static String getNameFromDB(String tableName, String column, int valueToSearch, DatabaseHandler dbh){
+		ResultSet rs;
+		String retString = "";
+		try {
+			rs=dbh.executeFormatQuery(tableName, new String[]{column}, "WHERE id"+tableName+" ='"+valueToSearch+"'");
+			if(rs.first()){
+				retString = rs.getString(1);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return retString;
+		
+	}
+
 }
