411185c5998fbe80e40311a23847111ccdc650b9
yuvalb <drakemare@gmail.com>
2015-06-14 17:51:58 +0300

    more shiat

--- src/GUI/Scenes/GameScene.java
+++ src/GUI/Scenes/GameScene.java
@@ -310,4 +310,38 @@ public class GameScene extends Scene{
 		}
 
 	}
+	
+	class NextListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
